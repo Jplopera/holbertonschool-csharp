a44389e7b38b41e8eb96194f6b2038584009d4c3
Juan Pablo Lopera Tobón <98365433+Jplopera@users.noreply.github.com>
2023-07-29 19:55:02 -0500

    If else loops update

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decNum;
+            for (decNum = 0; decNum < 99; decNum++)
+            {
+                Console.Write("{0} = 0x{0:x}\n", decNum);
+            }
+        }
+    }
+}
