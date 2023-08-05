6f727bb363cf310758261c0a338d366dba7631e2
Juan Pablo Lopera Tobón <98365433+Jplopera@users.noreply.github.com>
2023-08-04 19:00:31 -0500

    Data Structures

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> newList = new List<int>();
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        for (int i = 0; i < size; i++)
+            newList.Add(i);
+
+        Console.WriteLine("{0}", string.Join(" ", newList));
+
+        return newList;
+    }
+}
