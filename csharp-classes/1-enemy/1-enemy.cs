1b103286df3b9be3e289b1d7e85eab5754eaf71e
Juan Pablo Lopera Tobón <98365433+Jplopera@users.noreply.github.com>
2023-08-26 18:47:17 -0500

    Classes first commit

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// An class that defines a zombie.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// A public field that is an int with no value.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// A public constructor that sets the value of health to 0.
+        /// </summary>
+        public Zombie() => health = 0;
+    }
+}
---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enemies
+{
+    /// <summary>
+    /// An class that defines a zombie.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// A public field that is an int with no value.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// A public constructor that sets the value of health to 0.
+        /// </summary>
+        public Zombie() => health = 0;
+
+        /// <summary>
+        /// A public constructor that throws an error if value less than 0.
+        /// </summary>
+        /// <param name="value">Must be greater than or equal to zero.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+    }
+}
